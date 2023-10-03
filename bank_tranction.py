ee070304ec919a607501678818ff6f6f9f20e933
tbhullar2 <tbhullar2@acadamic.rrc.ca>
2023-10-03 12:48:01 -0500

    just checking it's working or not

---
+++ bank_tranction.py
@@ -0,0 +1,10 @@
+"""
+Description: learning loops and logic in python
+Author: Tanvir Singh Bhullar
+Date: 27/09/2023
+Usage: just learning new stuff
+"""
+
+border=("*******************")
+print(border)
+
