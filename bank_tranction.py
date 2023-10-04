ac002295f2726756050735e3a450fc2128b48a2b
tbhullar2 <tbhullar2@acadamic.rrc.ca>
2023-10-04 17:11:41 -0500

    modified bank tranction

--- bank_tranction.py
+++ bank_tranction.py
@@ -5,44 +5,11 @@ Date: 27/09/2023
 Usage: just learning new stuff
 """
 
+list= ["D" , "W" , "Q"]
 import random
-import os
-from time import sleep
 
-variables={"D", "W", "Q"}
+random_number=random.randint(-1000, 10000)
+bank_balance=float(random_number)
+print(bank_balance)
 
-bank_balance=random.randint(-1000, 100000)
-user_balance= float(bank_balance)
-
-while True:
-    width= 40
-    fills="*"
-
-    print(fills * width)
-    print("PIXELL RIVER FINSNCIAL".center(width))
-    print("ATM SIMULATOR" .center(width))
-    print(f"your current balance is: ${user_balance:,.2f}")
-    print(f"Deposit: {variables[1]}".center(width))
-    print(f"Withdraw: {variables[2]}".center(width))
-    print(f"to Quit: {variables[3]}".center(width))
-    print(fills * width, "/n")
-
-    option=input("enter your selection:").upper()
-    if option=="Q":
-        print("Thank you for using this ATM. Have a great day!")
-        break
-    elif option=="D":
-        deposit_amount=float(input("enter amount of transaction: $"))
-        if deposit_amount<0:
-            print(fills * width)
-            print("Please enter a positive amount in it.")
-            print(fills * width)
-        else:
-            user_balance+=deposit_amount
-            print(fills * width)
-            print(f"Your current balance is:${user_balance:,.2f}".center(width))
-            print(fills * width)
-    elif option=="W":
-       
-
-
+print("*", 40)
